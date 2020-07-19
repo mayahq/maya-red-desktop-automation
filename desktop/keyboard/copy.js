bb9a52a65b56a99e24ab6eda0b320c964bbbe1d1
sibeshkar <sibeshkar@users.noreply.github.com>
2020-07-20 05:10:51 +0530

    copy + keyTap added

---
+++ desktop/keyboard/copy.js
@@ -0,0 +1,41 @@
+var robot = require("robotjs");
+var clipboard = require("copy-paste");
+module.exports = function (RED) {
+  function DesktopKeyboardCopy(config) {
+    RED.nodes.createNode(this, config);
+    this.name = config.name;
+
+    var node = this;
+
+    // Retrieve the config node
+    this.on("input", function (msg) {
+      node.status({
+        fill: "yellow",
+        shape: "dot",
+        text: "copying",
+      });
+      robot.keyTap("c", ["command"]);
+      clipboard.paste(function (err, p) {
+        if (err) {
+          node.status({
+            fill: "red",
+            shape: "ring",
+            text: "error: " + err.toString().substring(0, 10) + "...",
+          });
+        } else {
+          node.status({
+            fill: "greed",
+            shape: "ring",
+            text: "copied!",
+          });
+          msg.copied = p;
+          node.send(msg);
+        }
+      });
+    });
+    oneditprepare: function oneditprepare() {
+      $("#node-input-name").val(this.name);
+    }
+  }
+  RED.nodes.registerType("desktop-keyboard-copy", DesktopKeyboardCopy);
+};
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // Move the mouse across the screen as a sine wave.
 var robot = require("robotjs");
 
-setTimeout(() => {
-  robot.keyTap("c", ["command"]);
-}, 3000);
+var ncp = require("copy-paste");
 
 setTimeout(() => {
-  robot.keyTap("v", ["command"]);
-  robot.keyTap("v", ["command"]);
-}, 6000);
+  robot.keyTap("c", ["command"]);
+  ncp.paste(function (err, p) {
+    console.log(p);
+  });
+}, 3000);
+
+// setTimeout(() => {
+//   ncp.paste(function (err, p) {
+//     console.log(p);
+//   });
+// }, 6000);
